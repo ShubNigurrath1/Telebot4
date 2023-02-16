036d5589e2a519239089b2aa3916fd31e8cd0731
Genry <zaskyuriy@gmail.com>
2023-02-16 20:47:23 +0700

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,100 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+
+public class Bot extends TelegramLongPollingBot {
+    final private String BOT_TOKEN = "5907762286:AAGBnokK8RigMYcReOeyI1pG8T1wR_XTPmw";
+    final private String BOT_NAME = "Telebot";
+    Storage storage;
+    Bot(){
+        storage=new Storage();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try{
+            if(update.hasMessage() && update.getMessage().hasText())
+            {
+                //Извлекаем из объекта сообщение пользователя
+                Message inMess = update.getMessage();
+                //Достаем из inMess id чата пользователя
+                String chatId = inMess.getChatId().toString();
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                String response = parseMessage(inMess.getText());
+                //Создаем объект класса SendMessage - наш будущий ответ пользователю
+                SendMessage outMess = new SendMessage();
+                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+                replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
+                replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
+
+                //Создаем список с рядами кнопок
+                ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+                //Создаем один ряд кнопок и добавляем его в список
+                KeyboardRow keyboardRow = new KeyboardRow();
+                keyboardRows.add(keyboardRow);
+                //Добавляем одну кнопку с текстом "Просвяти" наш ряд
+                keyboardRow.add(new KeyboardButton("Просвяти"));
+                //добавляем лист с одним рядом кнопок в главный объект
+                replyKeyboardMarkup.setKeyboard(keyboardRows);
+
+                //Добавляем в наше сообщение id чата а также наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+                outMess.setReplyMarkup(replyKeyboardMarkup);
+
+                //Отправка в чат
+                execute(outMess);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public String parseMessage(String textMsg) {
+        String response;
+
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if(textMsg.equals("/start"))
+            response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
+        else if(textMsg.equals("/get")||textMsg.equals("Просвяти"))
+            response = storage.getRandQuote();
+        else
+            response = "Сообщение не распознано";
+
+        return response;
+    }
+    void initKeyboard()
+    {
+        //Создаем объект будущей клавиатуры и выставляем нужные настройки
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
+        replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
+
+        //Создаем список с рядами кнопок
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        //Создаем один ряд кнопок и добавляем его в список
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRows.add(keyboardRow);
+        //Добавляем одну кнопку с текстом "Просвяти" наш ряд
+        keyboardRow.add(new KeyboardButton("Просвяти"));
+        //добавляем лист с одним рядом кнопок в главный объект
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+}
---
+++ src/main/java/Storage.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Storage {
+    private ArrayList<String> quoteList;
+    Storage()
+    {
+        quoteList = new ArrayList<>();
+        quoteList.add("Начинать всегда стоит с того, что сеет сомнения. \n\nБорис Стругацкий.");
+        quoteList.add("80% успеха - это появиться в нужном месте в нужное время.\n\nВуди Аллен");
+        quoteList.add("Мы должны признать очевидное: понимают лишь те,кто хочет понять.\n\nБернар Вербер");
+    }
+
+    String getRandQuote()
+    {
+        //получаем случайное значение в интервале от 0 до самого большого индекса
+        int randValue = (int)(Math.random() * quoteList.size());
+        //Из коллекции получаем цитату со случайным индексом и возвращаем ее
+        return quoteList.get(randValue);
+    }
+}
