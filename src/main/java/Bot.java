c6e14c8c1c6b3849b39f091aaeeedae1d69ffc2b
Genry <zaskyuriy@gmail.com>
2023-02-23 21:11:18 +0700

    23/02/2023

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,15 +1,22 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
+
     final private String BOT_TOKEN = "5907762286:AAGBnokK8RigMYcReOeyI1pG8T1wR_XTPmw";
     final private String BOT_NAME = "Telebot";
     Storage storage;
@@ -29,72 +36,80 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+
         try{
-            if(update.hasMessage() && update.getMessage().hasText())
+            if(update.hasMessage()&&update.getMessage().hasText())
             {
                 //Извлекаем из объекта сообщение пользователя
                 Message inMess = update.getMessage();
                 //Достаем из inMess id чата пользователя
                 String chatId = inMess.getChatId().toString();
-                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик,который возвращает нам ответ в зависимости от сообщения пользователя
                 String response = parseMessage(inMess.getText());
                 //Создаем объект класса SendMessage - наш будущий ответ пользователю
                 SendMessage outMess = new SendMessage();
-                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-                replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
-                replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
-
-                //Создаем список с рядами кнопок
-                ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
-                //Создаем один ряд кнопок и добавляем его в список
-                KeyboardRow keyboardRow = new KeyboardRow();
-                keyboardRows.add(keyboardRow);
-                //Добавляем одну кнопку с текстом "Просвяти" наш ряд
-                keyboardRow.add(new KeyboardButton("Просвяти"));
-                //добавляем лист с одним рядом кнопок в главный объект
-                replyKeyboardMarkup.setKeyboard(keyboardRows);
-
+/////////////////////////////// //СОЗДАЁМ inline-КЛАВИАТУРУ//////////////////////////////////////////////////
+                InlineKeyboardMarkup container=new InlineKeyboardMarkup();
+                List<InlineKeyboardButton>KeyLine=new ArrayList<>();
+                List<List<InlineKeyboardButton>>MyKeyBoard=new ArrayList<>();
+                InlineKeyboardButton MyBut1=new InlineKeyboardButton("BUT");
+                MyBut1.setCallbackData("11");
+                InlineKeyboardButton MyBut2=new InlineKeyboardButton("BUT2");
+                MyBut2.setCallbackData("22");
+                InlineKeyboardButton MyBut3=new InlineKeyboardButton("BUT3");
+                MyBut3.setCallbackData("33");
+                InlineKeyboardButton MyBut4=new InlineKeyboardButton("BUT4");
+                MyBut4.setCallbackData("44");
+                KeyLine.add(MyBut1);
+                KeyLine.add(MyBut2);
+                KeyLine.add(MyBut3);
+                KeyLine.add(MyBut4);
+                MyKeyBoard.add(KeyLine);
+                container.setKeyboard(MyKeyBoard);
+///////////////////////////////////СОЗДАЛИ КЛАВИАТУРУ///////////////////////////////////////////////////////////////////////
                 //Добавляем в наше сообщение id чата а также наш ответ
                 outMess.setChatId(chatId);
                 outMess.setText(response);
-                outMess.setReplyMarkup(replyKeyboardMarkup);
-
-                //Отправка в чат
+                outMess.setReplyMarkup(container);
                 execute(outMess);
             }
+            else if (update.hasCallbackQuery()){
+                SendMessage sendMessage=new SendMessage();
+                CallbackQuery callbackQuery=update.getCallbackQuery();
+                String data=callbackQuery.getData();
+                User user=callbackQuery.getFrom();
+                Message message=callbackQuery.getMessage();
+                if(data.equals("22")){
+
+                    // sendMessage.setParseMode("Markdown");
+                    sendMessage.setText("Получилось");
+                    sendMessage.setChatId(message.getChatId().toString());
+                    try {
+                        execute(sendMessage);}
+                    catch (TelegramApiException e1){
+                        e1.printStackTrace();
+                    }
+                }
+
+            }
+
         } catch (TelegramApiException e) {
             e.printStackTrace();
-        }
 
+
+
+        }
     }
     public String parseMessage(String textMsg) {
         String response;
-
         //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
         if(textMsg.equals("/start"))
-            response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
-        else if(textMsg.equals("/get")||textMsg.equals("Просвяти"))
+            response = "Привет,я знаю много цитат.Жми /get";
+        else if(textMsg.equals("/get"))
             response = storage.getRandQuote();
+
         else
             response = "Сообщение не распознано";
-
         return response;
-    }
-    void initKeyboard()
-    {
-        //Создаем объект будущей клавиатуры и выставляем нужные настройки
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
-        replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
-
-        //Создаем список с рядами кнопок
-        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
-        //Создаем один ряд кнопок и добавляем его в список
-        KeyboardRow keyboardRow = new KeyboardRow();
-        keyboardRows.add(keyboardRow);
-        //Добавляем одну кнопку с текстом "Просвяти" наш ряд
-        keyboardRow.add(new KeyboardButton("Просвяти"));
-        //добавляем лист с одним рядом кнопок в главный объект
-        replyKeyboardMarkup.setKeyboard(keyboardRows);
     }
 }
